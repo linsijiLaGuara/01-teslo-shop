e767cf22fe4fd85194900ce1db67d48c2a512360
Yoselin Silva Jimenez <ysilvaj@MacBook-Pro-de-RI710472.local>
2022-11-22 11:37:59 -0300

    add: DTO

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,37 @@
-export class CreateProductDto {}
+export class CreateProductDto {
+
+    @IsString()
+    @MinLength(1)
+    title: string;
+
+    @IsNumber()
+    @IsPositive()
+    @IsOptional()
+    price?: number;
+
+    @IsString()
+    @IsOptional()
+    description?: string;
+
+    @IsString()
+    @IsOptional()
+    slug?: string;
+
+    @IsInt()
+    @IsPositive()
+    @IsOptional()
+    stock?: number; 
+
+    @IsString({ each: true })
+    @IsArray()
+    sizes: string[]
+
+    @IsIn(['men','women','kid','unisex'])
+    gender: string;
+
+    @IsString({ each: true })
+    @IsArray()
+    @IsOptional()
+    tags: string[];
+
+}
