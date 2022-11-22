392c3b7515268af8371e07bb41686de825b70c0d
Yoselin Silva Jimenez <ysilvaj@MacBook-Pro-de-RI710472.local>
2022-11-22 11:52:40 -0300

    add: guarde en un servidor

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,11 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
+import { ProductsController } from './products.controller';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+
+constructor(
+  @InjectRepository(Product)
+  private readonly producRespository: Repository<Product>,
+
+){}
+
+
+  async create(createProductDto: CreateProductDto) {
+    try {
+      const product = this.producRespository.create(createProductDto);
+      await this.producRespository.save(product);
+    } catch (error) {
+      console.log(error)
+      throw new InternalServerErrorException('ayuda!')
+      
+      
+    }
   }
 
   findAll() {
